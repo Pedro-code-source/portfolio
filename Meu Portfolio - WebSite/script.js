e367e4c1a5ee6893c835f58045e25d30ffab770c
Pedro-hub-source <pedropereiraa.work@gmail.com>
2025-08-20 15:20:11 -0300

    finalizada a parte "grosseira" do projeto

--- Meu Portfolio - WebSite/script.js
+++ Meu Portfolio - WebSite/script.js
@@ -1,9 +1,36 @@
-const toggleMenuButton = document.querySelector('#menu-toggle');
+const comentario = document.getElementById("comentario");
+const botaoEnviarComentario = document.getElementById("botao-enviar-comentario")
+const listaDeComentarios = document.getElementById("lista-de-comentarios")
+const com = document.getElementById("comentarios")
+let array = {};
 
-const menuLinks = document.querySelectorAll('.menu-links')
+if(localStorage.getItem("comentarios") !== null){
+    array = JSON.parse(localStorage.getItem("comentarios"))
+}
+if(listaDeComentarios){
+    for(const chave in array){
+        const novoItem = document.createElement("li")
+        const novaDiv = document.createElement("div")
+        const novoComentario = document.createElement("p")
+        const data = document.createElement("p")
 
-toggleMenuButton.addEventListener('click', () => {
-    for(let i = 0; i < menuLinks.length; i++) {
-        menuLinks[i].classList.toggle('show');
+        novoComentario.innerText = chave;
+        data.innerText = array[chave];
+        
+        novaDiv.appendChild(data)
+        novaDiv.appendChild(novoComentario)
+        novoItem.appendChild(novaDiv)
+        listaDeComentarios.appendChild(novoItem);
     }
-});
+}
+
+
+if (botaoEnviarComentario){
+    botaoEnviarComentario.addEventListener('click', ()=>{
+    array[comentario.value] = new Date();
+    localStorage.setItem("comentarios",JSON.stringify(array))
+    document.getElementById("comentario").value = "";
+    })
+}
+
+
